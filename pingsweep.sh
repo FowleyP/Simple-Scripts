581afb144f37fa63e39cde09047041d00e06c8ab
FowleyP <126918599+FowleyP@users.noreply.github.com>
2023-09-02 15:14:41 -0300

    Update pingsweep.sh

--- pingsweep.sh
+++ pingsweep.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <network>"
-    echo "Example: $0 192.168.1.0/24"
+    echo -e "\e[1;32m Usage: $0 <network>"
+    echo -e "\e[1;36m Example: $0 192.168.1.0/24"
     exit 1
 fi
 
 network="$1"
 
-echo "Scanning network: $network"
+echo -e "\e[1;32m Scanning network: \e[0;37m\e[1;37m$network\e[0;37m"
 
 for host in $(seq 1 254); do
     ip="${network%.*}.$host"
     ping -c 1 -w 1 "$ip" > /dev/null 2>&1
     if [ $? -eq 0 ]; then
-        echo "Host $ip is up"
+        echo -e "Host \e[1;37m$ip\e[0;37m is \e[1;31m up\e[0;37m"
     fi
 done
